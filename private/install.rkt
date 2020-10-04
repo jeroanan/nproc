20988914bb6a0dca7edad45d76f6be9b3e1de9e2
David Wilson <davidwil@posteo.de>
2020-10-04 11:54:10 +0100

    update install

--- private/install.rkt
+++ private/install.rkt
@@ -9,11 +9,12 @@
 (define (pre-installer collections-top-path racl-path)
   (define private-path (build-path racl-path "private"))
   (define unpacked-path (path->string (build-path private-path "gnulib-nproc")))
+  (define output-path (path->string (build-path private-path "gnulib-proc.so")))
   
   (untgz (build-path private-path "gnulib-nproc.tgz") #:dest private-path)
   (system (string-append "cd " unpacked-path " && ./configure && make"))
   (system (string-append
-           "gcc "unpacked-path"/gllib/*.o -I "unpacked-path" -shared -o private/gnulib-nproc.so"))
+           "gcc "unpacked-path"/gllib/*.o -I "unpacked-path" -shared -o "output-path))
   (delete-directory/files unpacked-path))
 
 
