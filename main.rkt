0c4adc290fe1fc4f23ae4f11f6f2a2dd407ad13f
David Wilson <davidwil@posteo.de>
2020-10-04 11:12:41 +0100

    Basically works

--- main.rkt
+++ main.rkt
@@ -1,50 +1,19 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+; Copyright 2020 David Wilson
+; See COPYING for details
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(provide get-number-of-processors)
 
-;; Code here
+(require ffi/unsafe
+         racket/runtime-path
+         (for-syntax racket/base))
 
+(define-runtime-path lib-path (build-path "private" "gnulib-nproc"))
+(define clib (ffi-lib lib-path))
 
+(define num-processors (get-ffi-obj "num_processors" clib (_fun _long -> _int)))
 
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
+(define (get-number-of-processors)
+  (num-processors 0))
 
-  (check-equal? (+ 2 2) 4))
-
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
--- private/install.rkt
+++ private/install.rkt
@@ -2,14 +2,20 @@
 
 (provide pre-installer)
 
-(require racket/system)
+(require racket/file
+         racket/system)
 (require file/untgz)
-
-(define unpacked-path "gnulib-nproc")
 
 (define (pre-installer collections-top-path racl-path)
   (define private-path (build-path racl-path "private"))
-  (untgz (build-path private-path "gnulib-nproc.tgz"))
-  (system "gnulib-nproc/configure"))
+  (define unpacked-path (path->string (build-path private-path "gnulib-nproc")))
+  
+  (untgz (build-path private-path "gnulib-nproc.tgz") #:dest private-path)
+  (system (string-append "cd " unpacked-path " && ./configure && make"))
+  (system (string-append
+           "gcc "unpacked-path"/gllib/*.o -I "unpacked-path" -shared -o private/gnulib-nproc.so"))
+  (delete-directory/files unpacked-path))
+
+
   
   
